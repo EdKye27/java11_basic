77c9ce7d5af39b90f667ce4ddced240dbee4cd14
EdKye27 <sungwookye@gmail.com>
2024-02-23 10:01:33 +0900

    0223 10:01

---
+++ Test05/src/org/KH/object1/Person.java
@@ -0,0 +1,12 @@
+package org.KH.object1;
+
+public class Person {
+	String name;
+	int year;
+	String gender;
+	String job;
+	void running()	{
+		System.out.println(this.name+" 이(가) 달립니다.");
+	}
+}
+
---
+++ Test05/src/org/KH/object1/PersonEx1.java
@@ -0,0 +1,14 @@
+package org.KH.object1;
+
+public class PersonEx1 {
+
+	public static void main(String[] args) {
+		Person kye = new Person();
+		kye.name = "계성우";
+		kye.year = 1999;
+		kye.gender = "남성";
+		kye.job = "대학생";
+		kye.running();
+	}
+
+}
